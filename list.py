7b6ab3d748be60729cf8236177d994088651afd2
vishal-kr72 <143835757+vishal-kr72@users.noreply.github.com>
2025-04-05 19:43:14 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,11 @@
+l1 = [1,2,3,4,5,6,7,8,9,90]
+l1.append(20)
+l2 = ["apple,","orange"]
+  # in list we can use indexing and sliding techniq to jump the list number .
+print(l1[0:10:2]) # Positive indexing 
+print(l1[0:10])
+print(l1[0:10:3])
+print(l1[0:10:1])
+print(l1[-1:-10:2]) #negtive indexing
+print(l1)   # print all list element
+print(l1+l2)  # concatenation of two list
